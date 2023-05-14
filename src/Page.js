13a3331578934fe3310fee5f7c16b79f05223026
Sovlus <kubbaz122@gmail.com>
2023-05-14 12:53:03 +0200

    images + usestate

--- src/Page.js
+++ src/Page.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { useState } from 'react';
 
+// images without database / firebase
 
+const images = [
+    "https://images.pexels.com/photos/60130/pexels-photo-60130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3074526/pexels-photo-3074526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2908852/pexels-photo-2908852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13884371/pexels-photo-13884371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/10620753/pexels-photo-10620753.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+];
 
+export default function App() {
+    const [current, setCurrent] = useState(0);
+}
 
 
 
